0069df25dcbac1d3166565688112262dd5cd236a
Abel Salgado Romero <abelromero@gmail.com>
2021-07-09 00:01:41 +0200

    Add kid and typ to JWS tokens

--- src/main/java/org/bcnjug/jbcn/api/auth/JwtGenerator.java
+++ src/main/java/org/bcnjug/jbcn/api/auth/JwtGenerator.java
@@ -1,9 +1,6 @@
 package org.bcnjug.jbcn.api.auth;
 
-import com.nimbusds.jose.JWSAlgorithm;
-import com.nimbusds.jose.JWSHeader;
-import com.nimbusds.jose.JWSObject;
-import com.nimbusds.jose.Payload;
+import com.nimbusds.jose.*;
 import com.nimbusds.jose.crypto.RSASSASigner;
 import com.nimbusds.jwt.JWTClaimsSet;
 import lombok.SneakyThrows;
@@ -14,8 +11,8 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
+import java.util.Base64;
 import java.util.Date;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -32,7 +29,7 @@ public class JwtGenerator {
 
     @SneakyThrows
     public String createJWS(String subject, Set<String> roles, long ttlMillis) {
-        
+
         LocalDateTime expirationTime = LocalDateTime.now().plus(ttlMillis, ChronoUnit.MILLIS);
         Instant instant = expirationTime.atZone(ZoneId.systemDefault()).toInstant();
 
@@ -45,12 +42,27 @@ public class JwtGenerator {
         if (!roles.isEmpty()) {
             claimsBuilder.claim("roles", roles.stream().sorted().collect(Collectors.toList()));
         }
-
-        JWSHeader header = new JWSHeader(JWSAlgorithm.RS256);
+        JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.RS256)
+                .type(JOSEObjectType.JWT)
+                .keyID(buildKid(privateKey.getEncoded()))
+                .build();
         JWSObject jwsObject = new JWSObject(header, new Payload(claimsBuilder.build().toJSONObject()));
         jwsObject.sign(new RSASSASigner(privateKey));
 
         return jwsObject.serialize();
+    }
+
+    private String buildKid(byte[] key) {
+        byte[] bytes = new byte[8];
+        bytes[0] = key[0];
+        bytes[1] = key[1];
+        bytes[2] = key[2];
+        bytes[3] = key[3];
+        bytes[4] = key[4];
+        bytes[5] = key[5];
+        bytes[6] = key[6];
+        bytes[7] = key[8];
+        return Base64.getEncoder().encodeToString(bytes);
     }
 
 }
